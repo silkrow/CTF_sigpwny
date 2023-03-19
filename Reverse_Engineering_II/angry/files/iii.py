ac9210b9cd6ed9785ae7bcbfbcf1c7609ed0ceec
Erkai Yu <si1krow@outlook.com>
2023-03-19 11:14:15 -0500

    maybe some progress, idk

--- Reverse_Engineering_II/angry/files/iii.py
+++ Reverse_Engineering_II/angry/files/iii.py
@@ -2,7 +2,7 @@ import logging
 import angr
 import claripy
 
-logging.getLogger('angr.sim_manager').setLevel('DEBUG')
+# logging.getLogger('angr.sim_manager').setLevel('DEBUG')
 
 len_fail = 0x004052ec
 success = 0x0040547d
@@ -12,6 +12,7 @@ for_success = 0x00405465
 proj = angr.Project("./angry")
 
 arg1 = claripy.BVS('arg1', 31*8)
+
 state = proj.factory.entry_state(
 	args=['angry', arg1],
 	# add_options = angr.options.unicorn
@@ -23,16 +24,32 @@ for b in arg1.chop(8):
 
 sm = proj.factory.simulation_manager(state)
 
-sm.explore(find = success, avoid = fail)
+#### METHOD 1 ####
 
-print(sm.found[0].solver.eval(arg1, cast_to=bytes))
+#sm.explore(find = success, avoid = fail)
+#print(sm.found[0].solver.eval(arg1, cast_to=bytes))
 
-#for i in range(31):
-#	print(i)
-#	print(sm.active[0].solver.eval(arg1, cast_to=bytes))
-#	print(sm.active[0].regs.eax)
-#	sm.explore(find=for_success, avoid=fail).unstash(from_stash='found', to_stash='active')
-#s = sm.active[0]
-#print("Done!")
-#print(s.solver.eval(arg1,cast_to=bytes))
+#### END OF METHOD 1 ####
 
+#### METHOD 2 ####
+
+iter_num = 0
+
+def find_condition(state):
+	if (state.addr == for_success and state.solver.eval(state.regs.eax) == iter_num):
+		return True
+	else:
+		return False
+
+for i in range(31):
+	print(i)
+	print(sm.active[0].solver.eval(arg1, cast_to=bytes))
+	print(sm.active[0].regs.eax)
+	iter_num = i
+	sm.explore(find=find_condition, avoid=fail).move(from_stash='found', to_stash='active')
+
+s = sm.active[0]
+print("Done!")
+print(s.solver.eval(arg1,cast_to=bytes))
+
+#### END OF METHOD 2 ####
