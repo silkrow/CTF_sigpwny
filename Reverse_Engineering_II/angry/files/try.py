dc5c297adc936bdf7e314cacf5a4dca45f28608d
Erkai Yu <si1krow@outlook.com>
2023-03-20 13:56:09 -0500

    some progress

--- Reverse_Engineering_II/angry/files/try.py
+++ Reverse_Engineering_II/angry/files/try.py
@@ -1,3 +1,20 @@
 import gdb
 
-gdb.execute("b *0x0040544f")
+
+for_success = 0x00405465
+fail = 0x0040544f
+for_update = 0x00405468
+
+
+gdb.execute("file angry")
+gdb.execute("set args s234567890123456789012345678901")
+gdb.execute("b *{}".format(for_update))
+gdb.execute("r")
+s = gdb.execute("p $eax", False, True)
+# parsing
+num = s.split("=")
+s = num[1].split()
+eax = int(s[0], 0)
+print(eax)
+
+
