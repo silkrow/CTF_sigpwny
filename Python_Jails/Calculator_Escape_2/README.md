f47dbf067aa7a1c11f85da80d64a3eaae9e97af1
Erkai Yu <si1krow@outlook.com>
2023-06-29 20:47:22 -0500

    cal2 added

---
+++ Python_Jails/Calculator_Escape_2/README.md
@@ -0,0 +1,7 @@
+---
+Added Time: June 29 2023
+Difficulty: easy
+Score: 125
+Link: https://ctf.sigpwny.com/challenges#Meetings/Calculator%20Escape%202-724
+---
+nc chal.sigpwny.com 7003
---
+++ Python_Jails/Calculator_Escape_2/files/chal.py
@@ -0,0 +1,25 @@
+# Your goal is to read the flag at /flag.txt.
+# For example, __import__('os').system('cat /flag.txt')
+# or print(open('/flag.txt').read()).
+
+def is_input_bad(s):
+    # Surely only math expressions will fit in this small
+    # string, right?
+    return len(s) > 15
+
+
+def main():
+    print('Welcome to my calculator!')
+    print('Enter your expression and I will evaluate it for you.')
+    print('> ', end='')
+    s = input()
+
+    if is_input_bad(s):
+        print("Hey what do you think you're doing?!")
+        return
+
+    print('Answer: {}'.format(eval(s)))
+
+
+if __name__ == '__main__':
+    main()
